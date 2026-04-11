7d6d9255af4a88cc46d41094d7bfb8b68bbb4332
CoburnFuse <dev@coburnius.net>
2026-04-11 11:57:42 +0200

    updated the build script to have a proper way of initializing variables, and updated the "Poi Sound" function to be an event listener

--- build.js
+++ build.js
@@ -1,41 +1,36 @@
-var fs = require('fs');
-var root = __dirname;
+const fs = require('fs');
+const root = __dirname;
 
 //Set folders
-var inputFolder = root + "/pages/";
-var outputFolder = root + "/dist/";
-var componentsFolder = root + "/components/";
+const inputFolder = root + "/pages/";
+const outputFolder = root + "/dist/";
+const componentsFolder = root + "/components/";
 
 function replaceContents() {
     
     //Create components
-    var componentNavbar = fs.readFileSync(componentsFolder + "/navbar.html", 'utf-8');
-    var componentHeader = fs.readFileSync(componentsFolder + "/header.html", 'utf-8');
+    const componentNavbar = fs.readFileSync(componentsFolder + "/navbar.html", 'utf-8');
+    const componentHeader = fs.readFileSync(componentsFolder + "/header.html", 'utf-8');
 
     //Copy everything from the pages folder and put it in the dist folder
     fs.cpSync(inputFolder, outputFolder, {recursive:true});
 
     //Put everything in an array to go through
-    var allFilesInFolders = fs.readdirSync(outputFolder, {recursive:true})
+    const allFilesInFolders = fs.readdirSync(outputFolder, {recursive:true})
 
     //Go through everything in the dist folder and make changes as needed
     allFilesInFolders.forEach(function (file){
 
         //Get the first folder (or file) for the navbar
-        var currentNav = file.split(/[./\\]/)[0];
+        let currentNav = file.split(/[./\\]/)[0];
 
         //Skip anything that isnt an html file
         if(file.endsWith('.html')){
-            var fileContents = fs.readFileSync(outputFolder + file, 'utf-8');
+            let fileContents = fs.readFileSync(outputFolder + file, 'utf-8');
 
             //Makes a copy of the navbar so it can be re-used
-            var navbarToAdd = componentNavbar;
+            let navbarToAdd = componentNavbar;
             navbarToAdd = navbarToAdd.replace(`href='/${currentNav}.html'`, `href='/${currentNav}.html' id="currentPage"`);
-
-            //Add the Queen of Poi to the homepage
-            if(currentNav == "index"){
-                navbarToAdd = navbarToAdd.replace(`id="currentPage"`, `id="currentPage" onclick="playPoi()"`);
-            }
 
             //Replaces stuff and saves it to the dist folder
             fileContents = fileContents.replace("<!-- NAVBAR_INCLUDE -->", navbarToAdd);
--- pages/assets/scripts/index.js
+++ pages/assets/scripts/index.js
@@ -1,7 +1,19 @@
+var audioPoi = new Audio('/assets/audio/poi.mp3')
+
 function updateTime() {
     const now = new Date();
     document.querySelector("#time").textContent = dateTimeFormat(now, false);
 }
+
+function playPoi(){
+    event.preventDefault();
+    audioPoi.play();
+}
+
+currentPage.addEventListener("click", function (e) {
+    e.preventDefault();
+    playPoi();
+});
 
 updateTime();
 setInterval(updateTime, 1000);
