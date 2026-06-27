67534e33db4905838ecc1ee8f06c7d0309171086
CoburnFuse <dev@coburnius.net>
2026-06-27 17:59:13 +0200

    Added comments to the gallery script

--- pages/assets/scripts/gallery.js
+++ pages/assets/scripts/gallery.js
@@ -1,3 +1,4 @@
+//Creates the variables required to run the script
 let output = [];
 const galleryContainer = document.getElementById("gallery");
 
@@ -6,14 +7,19 @@ async function getData(root) {
     //Reads and encodes the location, and decodes the root part because it is stupid and cant read URLs properly
     const jsonLocation = encodeURI(decodeURI(root) + "files.json");
 
+    //Attempts to fetch the JSON file containing the file names
     try {
         const response = await fetch(jsonLocation);
+
+        //Gives an error if the file fails to load
         if (!response.ok) {
             throw new Error(`Response status: ${response.status}`);
         }
 
+        //Turns the JSON file into an array
         output = await response.json();
 
+        //Creates the gallery
         createGallery(output, root);
 
     } catch (error) {
@@ -22,11 +28,14 @@ async function getData(root) {
 }
 
 function createGallery(images, root){
+
+    //Goes through each file in the array and puts them in the gallery element
     images.forEach(element => {
+
+        //Adds the root to it, otherwise it would be just the file name, and then adds it to the element
         element = root + element;
         galleryContainer.innerHTML += `<a href="${element}"><img src="${element}"></a>`;
     });
-    
 }
 
 //Loads the content, depending on the data provided on the page
